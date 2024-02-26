43f13c9a7cc4b3e0e1aa7bb27872a2049d87dd58
Shota Ruo <scruo@ucdavis.edu>
2024-02-26 15:50:01 -0800

    added new component linkedln

---
+++ src/components/linkedln.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Linkedln = () => {
+    return (
+        <div>
+            <h1 style={{color: 'black'}}>Here is the link below:</h1>
+            <a href="linkedin.com/in/shota-ruo-248b9b2b6">Linkedln</a>
+        </div>
+    )
+}
