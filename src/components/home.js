b22be028ee26ad27ac633044cf7668c413d924bd
Shota Ruo <scruo@ucdavis.edu>
2024-12-28 16:40:17 -0800

    home page almost complete

--- src/components/home.js
+++ src/components/home.js
@@ -3,49 +3,46 @@ import '../App.css';
 import '../styles/home.css'
 
 export const Home = () => {
-    const [backgroundColor, setBackgroundColor] = useState('#eee8aa'); // Initial background color
-    
 
-    // function to generate a random color every time I click on button//useState
-    const generateRandomColor = () => {
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); // generating a random hex color creates a
-        //random number for it
-        setBackgroundColor(randomColor);
-    };
+
+
+
 
     return (
-        <div className="home-container" style={{backgroundColor}}>
-            <h2 id='homePageText'>I am Shota Ruo</h2>
-            <div className ='home-container2'>
-            <p id='webDevText'>Hello. This is Shota; an aspiring web developer.</p>
-            
-            <div className='flip-card'>
-                <div className='flip-card-inner'>
-                    <div className='flip-card-front'>
-                        <img src="images/ShotaRuoMAIN.JPG" width={300} height={300} id='self-portrait' alt="self portrait"></img>
-                    </div>
+        <div className="home-container">
+            <h2 id='homePageText'>I am  Shota Ruo</h2>
+            <div className='home-container2'>
+                <div className='home-container2-1'>
+                    <p id='webDevText'>Hello. I am a software developer.</p>
+                </div>
+                <div className='home-container2-2'>
+                    <div className='flip-card'>
+                        <div className='flip-card-inner'>
+                            <div className='flip-card-front'>
+                                <img src="images/ShotaRuoMAIN.JPG" width={300} height={300} id='self-portrait' alt="self portrait"></img>
+                            </div>
 
-                    <div className='flip-card-back'>
+                            <div className='flip-card-back'>
 
-                        <p id='hobbies'>Hobbies I have aside from coding and academics:
-                            <li>Walking my dog</li>
-                            <li>Playing basketball with friends</li>
-                            <li>Going to the gym and working out</li>
-                            <li>Exploring outside and going on hikes</li>
-                        </p>
-                        
+                                <p id='hobbies'>Hobbies I have aside from coding and academics:
+                                    <li>Walking my dog</li>
+                                    <li>Playing basketball with friends</li>
+                                    <li>Going to the gym and working out</li>
+                                    <li>Exploring outside and going on hikes</li>
+                                </p>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-            
             </div>
-            </div>
-            <p>(replacement text fill below in column type text)</p>
-
-            
-            
-            
+            <p id="replacement-text">(replacement text fill below in column type text)</p>
         </div>
+
+
+
+
+
     );
 };
 //create button for toggling light and dark mode 
